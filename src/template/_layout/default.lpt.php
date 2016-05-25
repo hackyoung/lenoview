4f5b2351415c26ff87016466202dd7417421c03c
hackyoung <hackyoung@163.com>
2016-05-25 13:36:26 +0800

    fixed bug

--- src/template/_layout/default.lpt.php
+++ src/template/_layout/default.lpt.php
@@ -2,10 +2,13 @@
 <html>
     <head>
         <view name="_element.head" data="$__head__" />
-        <fragment name="css"></fragment>
+        <fragment name="css">
+        </fragment>
     </head>
     <body>
-        <fragment name="body"></fragment>
-        <fragment name="js"></fragment>
+        <fragment name="body">
+        </fragment>
+        <fragment name="js">
+        </fragment>
     </body>
 </html>
