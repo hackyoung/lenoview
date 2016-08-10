bcf2d0c9ecd6067445fd10cb84c9eb1d5c7db98b
hackyoung <hackyoung@163.com>
2016-08-10 15:01:03 +0800

    test

--- test/schild.lpt.php
+++ test/schild.lpt.php
@@ -3,9 +3,9 @@ $h = [1,1,1,1,1,1,1,1,];
 ?>
 <extend name="leno._layout.default">
     <fragment name="body">
-        hello world
-        <llist name="{$h}" id="i">
-            <view name="test.testjs" />
-        </llist>
+        <div name="hello">
+            <fragment name="body">
+            </fragment>
+        </div>
     </fragment>
 </extend>
--- test/sschild.lpt.php
+++ test/sschild.lpt.php
@@ -1,5 +1,8 @@
-<extend name="schild">
-	<fragment name="child">
-		sschild
+<extend name="test.schild">
+	<fragment name="body">
+        hello world
+        <llist name="{$h}" id="i">
+            <view name="test.testjs" />
+        </llist>
 	</fragment>
 </extend>
